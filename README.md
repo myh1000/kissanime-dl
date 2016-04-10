74b4889c13dc196424f28e999d5bb6b7ccfaf9cf
myh1000 <myh1000@gmail.com>
2016-04-10 01:00:35 -0700

    tfw

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kissanime-dl
 Small command-line program to download videos from KissAnime.com
+
+sike its been made already tfw
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kissanime_dl',
+    version='0.1dev',
+    url='https://github.com/myh1000/kissanime-dl',
+    author='Michael Huang',
+    author_email='myh1000@gmail.com',
+    packages=['kissanime_dl',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)
